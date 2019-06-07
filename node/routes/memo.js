15383839e3ad6fb7c67cd7f21b8ae102b01cd250
Kohei Yamamoto <kh2yam1@gmial.com>
2019-06-07 15:10:43 -0400

    Add update function

--- node/routes/memo.js
+++ node/routes/memo.js
@@ -4,6 +4,9 @@ var router = express.Router();
 var model = require('./model');
 var Memo = model.Memo;
 
+// ---- //
+// Read //
+// ---- //
 router.get('/', function(req, res, next) {
     Memo.find({}, function (err, result) {
         if (err) throw err;
@@ -12,6 +15,9 @@ router.get('/', function(req, res, next) {
     });
 });
 
+// ------ //
+// Create //
+// ------ //
 router.post('/', function(req, res, next) {
     const memo = new Memo({
         memo: req.body.memo
@@ -22,13 +28,37 @@ router.post('/', function(req, res, next) {
     res.redirect('/memo');
 });
 
+// ------ //
+// Delete //
+// ------ //
 router.get('/delete', function(req, res, next) {
     const id = req.query.id;
-    console.log(id);
     Memo.findByIdAndRemove(id, function(err) {
         if (err) throw err;
         res.redirect('/memo');
     });
+});
+
+// ------ //
+// Update //
+// ------ //
+router.get('/update', function(req, res, next) {
+    const id = req.query.id;
+    Memo.findById(id, function(err, result) {
+        if (err) throw err;
+        res.render('memoupdate', { 
+            id  : id,
+            memo: result['memo'] });
+    })
+});
+
+router.post('/update', function(req, res, next) {
+    const id   = req.query.id;
+    const memo = req.body.memo;
+    Memo.findByIdAndUpdate(id, { memo: memo }, function(err) {
+        if (err) throw err;
+        res.redirect('/memo');
+    })
 });
 
 module.exports = router;
